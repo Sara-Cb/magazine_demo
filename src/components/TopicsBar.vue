9ca2b407424ea8af040346c921b301e7dd3310e4
SaraCb-Mercuri <saramaiden23@gmail.com>
2025-01-29 17:59:53 +0100

    last header piece

---
+++ src/components/TopicsBar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const topics = ref([
+    { title: 'Tutti i temi', id: 'all' },
+    { title: 'Ambiente', id: 'environment' },
+    { title: 'Economia', id: 'economy' },
+    { title: 'Mondo', id: 'world' },
+    { title: 'Non Profit', id: 'non-profit' },
+    { title: 'Politica', id: 'politics' },
+    { title: 'Società', id: 'society' },
+    { title: 'Welfare', id: 'welfare' }
+])
+
+</script>
+
+<template>
+    <nav class="topicsContainer">
+        <RouterLink v-for="(item) in topics" :to="'/news/' + item.id" class="topic">
+            <h5>{{ item.title }}</h5>
+        </RouterLink>
+    </nav>
+</template>
