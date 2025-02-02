abb441b922d8c05628731732e0f09cf2514fa820
SaraCb-Mercuri <saramaiden23@gmail.com>
2025-02-02 22:30:25 +0100

    fix with redirect, from home

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/news/all',
+    },
+    {
       path: '/news/:topic?',
       name: 'news',
       component: NewsView,
